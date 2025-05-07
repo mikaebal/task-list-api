ab7c074702598a94f425b6f56649306dde4c2858
Mikaela Baluyot <mikaebal@gmail.com>
2025-05-07 00:06:54 -0700

    implemented wave 4: integrated slack api task bot

--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -3,6 +3,8 @@ from ..db import db
 from app.models.task import Task
 from .route_utilities import validate_model
 from datetime import datetime
+import requests, os
+
 
 bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
 
@@ -81,11 +83,27 @@ def delete_task(id):
 
 
 # PATCH /tasks/1/mark_complete
+# Slack API
 @bp.patch("/<id>/mark_complete")
 def mark_complete(id):
     task = validate_model(Task, id)
-    task.completed_at = datetime.now()
-    db.session.commit()
+    if task.completed_at is None:
+        task.completed_at = datetime.now()
+        db.session.commit()
+
+        slack_bot_token = os.environ.get("SLACK_BOT_TOKEN")
+        slack_channel = os.environ.get("SLACK_CHANNEL", "task-notifications")
+
+        headers = {
+                "Authorization": f"Bearer {slack_bot_token}"
+            }
+
+        payload = {
+                "channel": slack_channel,
+                "text": f"Someone just completed the task {task.title}"
+            }
+
+        requests.post("https://slack.com/api/chat.postMessage", headers=headers, json=payload)
 
     return Response(status=204, mimetype="application/json")
 
